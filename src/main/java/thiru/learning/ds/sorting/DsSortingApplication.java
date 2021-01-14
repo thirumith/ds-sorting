1267790c9854eb754f916dcf13775a56bf8755f4
Thiru <dmthiruna@gmail.com>
2021-01-14 06:55:15 +0530

    Initial Commit

---
+++ src/main/java/thiru/learning/ds/sorting/DsSortingApplication.java
@@ -0,0 +1,13 @@
+package thiru.learning.ds.sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thiru/learning/ds/sorting/DsSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package thiru.learning.ds.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
